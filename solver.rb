b998a555ccac4425e90e20628d4e4811e6de6894
Fernando Salas <luisfernandosalasg@gmail.com>
2022-07-27 14:46:59 -0500

    Add Factorial method

--- solver.rb
+++ solver.rb
@@ -1,3 +1,7 @@
 class Solver
-  
+  def factorial(num)
+    raise ArgumentError, 'Wrong type of argument' if !num.is_a?(Integer) || num.negative?
+    return 1 if num == 0
+    num * factorial(num - 1)
+  end
 end
