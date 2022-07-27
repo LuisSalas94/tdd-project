47aa93c69590d87b9415a2c09cd8d28afc2b2fba
Fernando Salas <luisfernandosalasg@gmail.com>
2022-07-27 15:22:41 -0500

    Add Fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,7 +4,7 @@ describe Solver do
     @solver = Solver.new
   end
 
-  describe '#new' do 
+  describe '#new' do
     it 'should return a Solver object' do
       expect(@solver).to be_an_instance_of Solver
     end
@@ -22,20 +22,19 @@ describe Solver do
     it 'should raise an error for negative numbers' do
       expect { Solver.new.factorial(-1) }.to raise_error(ArgumentError)
     end
-
   end
 
   describe '#reverse' do
-   it 'Should return the reversed string' do
-     expect(@solver.reverse('hola')).to eql('aloh')
-   end
- 
-   it 'Should raise an exception if is not a string' do
-     expect { Solver.new.reverse(2) }.to raise_error(ArgumentError)
-   end
- end
+    it 'Should return the reversed string' do
+      expect(@solver.reverse('hola')).to eql('aloh')
+    end
 
- describe "#fizzbuzz" do
+    it 'Should raise an exception if is not a string' do
+      expect { Solver.new.reverse(2) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#fizzbuzz" do
     it "Should return the result" do
       expect(@solver.fizzbuzz(15)).to eql("fizzbuzz")
     end
@@ -53,5 +52,4 @@ describe Solver do
       expect { Solver.new.fizzbuzz("hola") }.to raise_error(ArgumentError)
     end
   end
-
 end
