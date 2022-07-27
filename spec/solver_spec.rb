b848e13902ae942ba2d4af3f6f210fdf22211d21
Fernando Salas <luisfernandosalasg@gmail.com>
2022-07-27 14:43:55 -0500

    Add Factorial test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -10,4 +10,20 @@ describe Solver do
     end
   end
 
+  describe '#factorial' do
+    it 'should return the factorial of the given number' do
+      expect(@solver.factorial(5)).to eq(120)
+    end
+
+    it 'should return 1 for 0' do
+      expect(@solver.factorial(0)).to eq(1)
+    end
+
+    it 'should raise an error for negative numbers' do
+      expect { Solver.new.factorial(-1) }.to raise_error(ArgumentError)
+    end
+
+  end
+
+
 end
